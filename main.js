04e3af13b4b225d93593fe8a6d4817bcade63b96
chaima-al <82261425+chaima-al@users.noreply.github.com>
2021-07-28 20:57:25 +0100

    climatiseures

--- main.js
+++ main.js
@@ -25,3 +25,13 @@ const navSlide = () => {
   
   }
   navSlide();
+  var counter=1;
+  setInterval(function(){
+     document.getElementById('radio' + counter).checked = true;
+     counter++; 
+    if(counter>5){
+      counter=1;
+
+    }
+
+  }, 6000);
